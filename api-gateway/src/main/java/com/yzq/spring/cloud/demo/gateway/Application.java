a35038975e3855505fa9dcb4634cba5f64d94b7f
yuanzhiqiang <xingyu_yzq@163.com>
2017-04-10 11:04:36 +0800

    add access filter

--- api-gateway/src/main/java/com/yzq/spring/cloud/demo/gateway/Application.java
+++ api-gateway/src/main/java/com/yzq/spring/cloud/demo/gateway/Application.java
@@ -3,11 +3,20 @@ package com.yzq.spring.cloud.demo.gateway;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.yzq.spring.cloud.demo.gateway.filter.AccessFilter;
 
 @EnableZuulProxy
 @SpringCloudApplication
 public class Application {
+	
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(Application.class).web(true).run(args);
+	}
+	
+	@Bean
+	public AccessFilter accessFilter(){
+		return new AccessFilter();
 	}
 }
